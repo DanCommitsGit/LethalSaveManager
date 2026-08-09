eccf2b2392f578daebfaddfca9f6dcbcc9edf725
Daniel <61391827+DanCommitsGit@users.noreply.github.com>
2026-08-08 18:09:13 -0700

    Cleanup unused code and assets

--- LCSecurity.cs
+++ LCSecurity.cs
@@ -38,41 +38,5 @@ namespace LethalSaveManager
 			}
 		}
 
-		public static byte[] Encrypt(string data)
-		{
-			using (Aes aesAlg = Aes.Create())
-			{
-				aesAlg.Mode = CipherMode.CBC;
-				aesAlg.Padding = PaddingMode.PKCS7;
-				aesAlg.KeySize = 128;
-
-				// Generate a random IV
-				aesAlg.GenerateIV();
-
-				using (Rfc2898DeriveBytes keyDerivation = new Rfc2898DeriveBytes(Password, aesAlg.IV, 100, HashAlgorithmName.SHA1))
-				{
-					aesAlg.Key = keyDerivation.GetBytes(16); // 128-bit key
-
-					using (MemoryStream encryptedStream = new MemoryStream())
-					{
-						using (CryptoStream cryptoStream = new CryptoStream(encryptedStream, aesAlg.CreateEncryptor(), CryptoStreamMode.Write))
-						{
-							byte[] dataBytes = Encoding.UTF8.GetBytes(data);
-							cryptoStream.Write(dataBytes, 0, dataBytes.Length);
-						}
-
-						byte[] iv = aesAlg.IV; // Get IV
-						byte[] encryptedData = encryptedStream.ToArray(); // Get encrypted data
-
-						// Combine IV and encrypted data
-						byte[] ivAndEncryptedData = new byte[iv.Length + encryptedData.Length];
-						Array.Copy(iv, 0, ivAndEncryptedData, 0, iv.Length);
-						Array.Copy(encryptedData, 0, ivAndEncryptedData, iv.Length, encryptedData.Length);
-
-						return ivAndEncryptedData;
-					}
-				}
-			}
-		}
 	}
 }
